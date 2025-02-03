b9632c632afaaaaa3dd7c7d7c13e42ef95261f0a
Manuj Bhardwaj <manujbharaseo@gmail.com>
2025-02-03 14:03:30 +0530

    change navbar header

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -30,7 +30,6 @@ function Navbar() {
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
           RALPH<br></br>EDWARDS.
-          <i class='fab fa-react'/>
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
