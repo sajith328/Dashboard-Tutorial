7ac7f96c4c729fe4aba32155c2b32d3341083f5f
KAISVHORA <kaisvhora70@gmail.com>
2025-02-04 15:58:43 +0530

    Change Name-pk

--- src/Components/Pages/Work.js
+++ src/Components/Pages/Work.js
@@ -8,38 +8,58 @@ import './Work.css';
 export const Home = ()  => (
     /* Home section */
 
-    
-        
-						
+    <div>
+        {/* Work-width */}
+        <div className='Work-width'>
+            <div className='container'>
+                <div className='container-1'>
+                    <div className='Work-content-photo'>
+                        <div className='Title-Nav'>
+                        <h1 className='About-Title'>Latest case study</h1>
+                        <ul className='Work-nav'>
+                            <nav className='Work-items'>
+                                <li className='#'>All</li>
+                                <li className='#'>UI Design</li>
+                                <li className='#'>Webflow</li>
+                                <li className='#'>Marketing</li>
+                                <li className='#'>Content</li>
+                            </nav>
+                        </ul>
+                        </div>
 
-
-            <div>  
-
-                    <div>
-                        
-                    <div  className="background">
-                    
-                    <h2 className="text1">Your Name</h2>
-
-                    <h2 className="text">React Developer</h2> 
+                        <div className='Work-Cards'>
+                            <div className='work-C-flex'>
+                            <img class="image-Scroll" src="/Images/Project (4).png"/>
+                            <img class="image-Scroll" src="/Images/Project (5).png"/>
+                            </div>
+                            <div className='work-C-flex'>
+                            <img class="image-Scroll" src="/Images/Project (3).png"/>
+                            <img class="image-Scroll" src="/Images/Dribbble (1).png"/>
+                            </div>
+                            <div className='work-C-flex'>
+                            <img class="image-Scroll" src="/Images/Dribbble (2).png"/>
+                            <img class="image-Scroll" src="/Images/Photo (2).png"/>
+                            </div>
+                            <div className='work-C-flex'>
+                            <img class="image-Scroll" src="/Images/Photo (1).png"/>
+                            <img class="image-Scroll" src="/Images/Project (1).png"/>
+                            </div>
+                            <div className='work-C-flex'>
+                            <img class="image-Scroll" src="/Images/Project (2).png"/>
+                            <img class="image-Scroll" src="/Images/Project.png"/>
+                            </div>
+                        </div>
+                        <div className='button-service'>
+                            <button className='button-primary button-work'>
+                            <i class="fa fa-briefcase"></i>Load more project...
+                            </button>
+                        </div>
                     </div>
-                    
-                    
-                    
-
-                    
-                    
-                        <Container>
-                            <Row>
-                                    
-                                    <ph className="text1">This website was built with react. </ph> 
-                                    <ph className="text1">To view my WordPress website visit www.yoursite.com. To see my website built
-                                     with HTML, CSS, and JavaScript visit github at https://your company.github.io/github.io/</ph> 
-                                    
-                            </Row>      
-                    </Container>
                 </div>
-</div>
+            </div>
+        </div>
+        {/* End Work-width */}
+    </div>
 		
 
 )
