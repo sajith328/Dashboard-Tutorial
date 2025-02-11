5f33af6c7fb9c886e7133c7d1b400ab9e060e741
Manuj Bhardwaj <manujbharaseo@gmail.com>
2025-02-11 15:46:34 +0530

    change a service

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -51,11 +51,11 @@ function Navbar() {
             </li>
             <li className='nav-item'>
               <Link
-                to='/CV'
+                to='/Services'
                 className='nav-links'
                 onClick={closeMobileMenu}
               >
-                CV
+                Blog
               </Link>
             </li>
             <li className='nav-item'>
--- src/Components/Pages/Services.js
+++ src/Components/Pages/Services.js
@@ -7,12 +7,242 @@ import './Services.css';
 export const Services = ()  => (
     /* Home section */
 
-    
+    <main className='main-blog'>
+        <div className='content-blog'>
+            <h1>Article & Blog</h1>
+            <p>Morbi risus elit, fringilla in cursus a, molestie non dui. Nunc accumsan gravida risus, ac semper libero mollis ut.</p>
+        </div>
+        <div className='blog-header'>
+            <div className='header-blog'>
+                <div className='card-grid-blog'>
+                    <div className='blog'>
+                        <div className='blog-box'>
+                            <img src='/Images/the ocean visit.png'/>
+                        </div>
+                        <div className='body-blog'>
+                            <div>
+                                <span>Dec 18, 2020</span>
+                                <span>5 min read</span>
+                            </div>
+                            <h1>Simple Design Tips for Crafting Better UI Cards</h1>
+                        </div>
+                        <div className='footer-blog'>
+                            <p>It’s a situation every designer knows; we work on a feature, we release it, but always we say to ourselves:</p>
+                            <a href='#'>Read More
+                                <img src='/Images/ArrowRight.png'/>
+                            </a>
+                        </div>
+                    </div>
+                    <div className='blog'>
+                        <div className='blog-box'>
+                            <img src='/Images/flower.png'/>
+                        </div>
+                        <div className='body-blog'>
+                            <div>
+                                <span>Nov 22, 2006</span>
+                                <span>5 min read</span>
+                            </div>
+                            <h1>How to choose the right colors when creating a website?</h1>
+                        </div>
+                        <div className='footer-blog'>
+                            <p>Many of us hope the PM will take care of it, and we will continue to improve the features we release.</p>
+                            <a href='#'>Read More
+                                <img src='/Images/ArrowRight.png'/>
+                            </a>
+                        </div>
+                    </div>
+                    <div className='blog'>
+                        <div className='blog-box'>
+                            <img src='/Images/we hands.png'/>
+                        </div>
+                        <div className='body-blog'>
+                            <div>
+                                <span>Sep 01, 2000</span>
+                                <span>5 min read</span>
+                            </div>
+                            <h1>Travelling as a way of self-discovery and progress</h1>
+                        </div>
+                        <div className='footer-blog'>
+                            <p>It’s a situation every designer knows; we work on a feature, we release it, but always we say to ourselves:</p>
+                            <a href='#'>Read More
+                                <img src='/Images/ArrowRight.png'/>
+                            </a>
+                        </div>
+                    </div>
+                    <div className='blog'>
+                        <div className='blog-box'>
+                            <img src='/Images/under fire.png'/>
+                        </div>
+                        <div className='body-blog'>
+                            <div>
+                                <span>Jul 11, 2015</span>
+                                <span>5 min read</span>
+                            </div>
+                            <h1>How to design your site footer like we did</h1>
+                        </div>
+                        <div className='footer-blog'>
+                            <p>It’s a situation every designer knows; we work on a feature, we release it, but always we say to ourselves:</p>
+                            <a href='#'>Read More
+                                <img src='/Images/ArrowRight.png'/>
+                            </a>
+                        </div>
+                    </div>
+                    <div className='blog'>
+                        <div className='blog-box'>
+                            <img src='/Images/tree.png'/>
+                        </div>
+                        <div className='body-blog'>
+                            <div>
+                                <span>Jan 31, 2000</span>
+                                <span>5 min read</span>
+                            </div>
+                            <h1>Why choose a theme that looks good with WooCommerce</h1>
+                        </div>
+                        <div className='footer-blog'>
+                            <p>It’s a situation every designer knows; we work on a feature, we release it, but always we say to ourselves:</p>
+                            <a href='#'>Read More
+                                <img src='/Images/ArrowRight.png'/>
+                            </a>
+                        </div>
+                    </div>
+                    <div className='blog'>
+                        <div className='blog-box'>
+                            <img src='/Images/gift.png'/>
+                        </div>
+                        <div className='body-blog'>
+                            <div>
+                                <span>Aug 27, 2019</span>
+                                <span>5 min read</span>
+                            </div>
+                            <h1>How to write content about your photographs</h1>
+                        </div>
+                        <div className='footer-blog'>
+                            <p>It’s a situation every designer knows; we work on a feature, we release it, but always we say to ourselves:</p>
+                            <a href='#'>Read More
+                                <img src='/Images/ArrowRight.png'/>
+                            </a>
+                        </div>
+                    </div>
+                    <div className='blog'>
+                        <div className='blog-box'>
+                            <img src='/Images/we with.png'/>
+                        </div>
+                        <div className='body-blog'>
+                            <div>
+                                <span>Feb 10, 2018</span>
+                                <span>5 min read</span>
+                            </div>
+                            <h1>Helping a local business reinvent itself</h1>
+                        </div>
+                        <div className='footer-blog'>
+                            <p>It’s a situation every designer knows; we work on a feature, we release it, but always we say to ourselves:</p>
+                            <a href='#'>Read More
+                                <img src='/Images/ArrowRight.png'/>
+                            </a>
+                        </div>
+                    </div>
+                    <div className='blog'>
+                        <div className='blog-box'>
+                            <img src='/Images/pic.png'/>
+                        </div>
+                        <div className='body-blog'>
+                            <div>
+                                <span>Mar 02, 2019</span>
+                                <span>5 min read</span>
+                            </div>
+                            <h1>Lessons and insights from 8 years of Pixelgrade</h1>
+                        </div>
+                        <div className='footer-blog'>
+                            <p>It’s a situation every designer knows; we work on a feature, we release it, but always we say to ourselves:</p>
+                            <a href='#'>Read More
+                                <img src='/Images/ArrowRight.png'/>
+                            </a>
+                        </div>
+                    </div>
+                    <div className='blog'>
+                        <div className='blog-box'>
+                            <img src='/Images/red.png'/>
+                        </div>
+                        <div className='body-blog'>
+                            <div>
+                                <span>Mar 22, 2013</span>
+                                <span>5 min read</span>
+                            </div>
+                            <h1>Caring is the new marketing</h1>
+                        </div>
+                        <div className='footer-blog'>
+                            <p>It’s a situation every designer knows; we work on a feature, we release it, but always we say to ourselves:</p>
+                            <a href='#'>Read More
+                                <img src='/Images/ArrowRight.png'/>
+                            </a>
+                        </div>
+                    </div>
+                    <div className='blog'>
+                        <div className='blog-box'>
+                            <img src='/Images/country.png'/>
+                        </div>
+                        <div className='body-blog'>
+                            <div>
+                                <span>Apr 09, 2011</span>
+                                <span>5 min read</span>
+                            </div>
+                            <h1>Where to grow your business as a photographer: site or social media?</h1>
+                        </div>
+                        <div className='footer-blog'>
+                            <p>It’s a situation every designer knows; we work on a feature, we release it, but always we say to ourselves:</p>
+                            <a href='#'>Read More
+                                <img src='/Images/ArrowRight.png'/>
+                            </a>
+                        </div>
+                    </div>
+                    <div className='blog'>
+                        <div className='blog-box'>
+                            <img src='/Images/she pic.png'/>
+                        </div>
+                        <div className='body-blog'>
+                            <div>
+                                <span>Sep 27, 2016</span>
+                                <span>5 min read</span>
+                            </div>
+                            <h1>How does writing influence your personal brand?</h1>
+                        </div>
+                        <div className='footer-blog'>
+                            <p>It’s a situation every designer knows; we work on a feature, we release it, but always we say to ourselves:</p>
+                            <a href='#'>Read More
+                                <img src='/Images/ArrowRight.png'/>
+                            </a>
+                        </div>
+                    </div>
+                    <div className='blog'>
+                        <div className='blog-box'>
+                            <img src='/Images/man pic.png'/>
+                        </div>
+                        <div className='body-blog'>
+                            <div>
+                                <span>Aug 28, 2004</span>
+                                <span>5 min read</span>
+                            </div>
+                            <h1>Starting your traveling blog with Vasco</h1>
+                        </div>
+                        <div className='footer-blog'>
+                            <p>It’s a situation every designer knows; we work on a feature, we release it, but always we say to ourselves:</p>
+                            <a href='#'>Read More
+                                <img src='/Images/ArrowRight.png'/>
+                            </a>
+                        </div>
+                    </div>
+
+                </div>
+                <button className='button-blog'>Load more blog...</button>
+            </div>
+            
+        </div>
+    </main>
         
 						
 
 
-            <div>  
+//             <div>  
 
                    
                     
@@ -21,60 +251,60 @@ export const Services = ()  => (
 
                     
                     
-<h2 className="section-title"> </h2>
+// <h2 className="section-title"> </h2>
 
                     
 
-                    <Container>
-                            <Row>
-                                <Col> <h1 className="icon-title" >Tools I use to build and maintain websites.</h1> </Col>  
-                                <Col><img class="image" src="/Images/React.png" /> </Col>
-                                <Col><img class="image" src="/Images/JavaScript.png" /> </Col>
-                                <Col><img class="image" src="/Images/WordPress.png" /> </Col>
-                                <Col><img class="image" src="/Images/HTML.png" /> </Col>
-                                <Col><img class="image" src="/Images/Bootstrap.png" /> </Col>
-                                <Col><img class="image" src="/Images/CSS.png" /> </Col>
-                            </Row>    
-                        </Container>
+//                     <Container>
+//                             <Row>
+//                                 <Col> <h1 className="icon-title" >Tools I use to build and maintain websites.</h1> </Col>  
+//                                 <Col><img class="image" src="/Images/React.png" /> </Col>
+//                                 <Col><img class="image" src="/Images/JavaScript.png" /> </Col>
+//                                 <Col><img class="image" src="/Images/WordPress.png" /> </Col>
+//                                 <Col><img class="image" src="/Images/HTML.png" /> </Col>
+//                                 <Col><img class="image" src="/Images/Bootstrap.png" /> </Col>
+//                                 <Col><img class="image" src="/Images/CSS.png" /> </Col>
+//                             </Row>    
+//                         </Container>
                
                 
-                    {/* Services section */}
-                    <h1 class="text-center">_____________________________________________________________</h1>
-                    <h2 className="section-title">Services I provide</h2>
-                    <h2 className="about-tom">I develop and maintain websites.  I am not satisfied until you are satisfied.    
+//                     {/* Services section */}
+//                     <h1 class="text-center">_____________________________________________________________</h1>
+//                     <h2 className="section-title">Services I provide</h2>
+//                     <h2 className="about-tom">I develop and maintain websites.  I am not satisfied until you are satisfied.    
 
-                </h2>
+//                 </h2>
                     
-                    <Container>      
-                            <Row>
-                                <section class="height-50vh center-aligned">
-                                    <img class="image" src="/Images/cartoon.jpg" />     
-                                </section>
-                            </Row> 
-                        </Container>
+//                     <Container>      
+//                             <Row>
+//                                 <section class="height-50vh center-aligned">
+//                                     <img class="image" src="/Images/cartoon.jpg" />     
+//                                 </section>
+//                             </Row> 
+//                         </Container>
                 
 
-                    <div className="body2">
+//                     <div className="body2">
                     
                     
                         
                         
 
                         
-                        <h1 class="text-center">_____________________________________________________________</h1>
+//                         <h1 class="text-center">_____________________________________________________________</h1>
 
-                {/* Services section */}
+//                 {/* Services section */}
 
-                        <h2 className="section-title"> </h2>
+//                         <h2 className="section-title"> </h2>
                     
                     
                     
                 
 
                     
-                    </div>
+//                     </div>
 
-                </div>
+//                 </div>
 
 		
 
